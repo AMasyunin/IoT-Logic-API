c1b2b0edd1c9b7a2686b8cf38b5bc259cabd5250
a.masyunin@squaregps.com <a.masyunin@squaregps.com>
2025-03-11 17:07:15 +0100

    Added draft structure for IoT Logic API documentation

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI3NTMyMQ/spectral.js?branch_slug=main&token=4110f288-897d-48f4-85e5-65045beca593";
+export default { extends: ruleset };
